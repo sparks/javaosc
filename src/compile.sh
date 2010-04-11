9a6df5854a27339da66f27b5a644362944d52dba
Severin Smith <severin@smallbutdigital.com>
2010-04-10 21:10:38 -0400

    Ant build file added

--- src/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ../bin;
-javac -d ../bin/ -classpath /Applications/Processing.app/Contents/Resources/Java/core.jar *.java utility/*.java;
-cd ../bin;
-jar -cf javaosc.jar javaosc;
-rm ../library/javaosc.jar;
-mv javaosc.jar ../library/;
-rm -r ../bin;
